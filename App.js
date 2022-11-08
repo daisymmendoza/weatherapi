f5c66c6778ac9a8b39220b10bdfd0c36103486bd
daisymmendoza <89948539+daisymmendoza@users.noreply.github.com>
2022-11-08 14:18:51 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,36 +3,25 @@ import React, { useState, useEffect} from 'react';
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <LoadWeather></LoadWeather>
     </div>
   );
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=a117932c0e837c0755546948f3765fad";
+function LoadWeather() {
+  const [currentWeather, setCurrentWeather] = useState({});
   
   useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=tolleson&units=imperial&appid=a117932c0e837c0755546948f3765fad`;
     fetch(url)
     .then ((resp) => resp.json())
-    .then ((data) => setCountries(data))
+    .then ((data) => setCurrentWeather(data))
   }, []);
   return (
-    <div className="countries">
-      <h3>Total Number of Countries: {countries.length}</h3>
-      {countries.map((country) => (
-        <Country country={country}></Country>
-      ))}
-    </div>
-  );
-}
-
-function Country(props) {
-  const {weather, main} = props.country;
-  return (
-    <div className="country">
-      <p>Country: {weather.main}</p>
-      <p>State: {main.temp}</p>
+    <div className="weather">
+      <p>{currentWeather.name}</p>
+      <p>Weather: {currentWeather.weather.main}</p>
+      <p>Temperature: {currentWeather.main.temp}</p>
     </div>
   );
 }
