4dca31d1f6891fe2876a8a9b3649d9a18b6b37bb
daisymmendoza <89948539+daisymmendoza@users.noreply.github.com>
2022-11-13 17:00:48 -0700

    Update App.js

--- App.js
+++ App.js
@@ -11,44 +11,45 @@ function App() {
 function LoadWeather() {
   const [currentWeather, setCurrentWeather] = useState({});
   const [getWeather, setGetWeather] = useState('');
-  const [state, setState] = useState('');
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${state}&units=imperial&appid=a117932c0e837c0755546948f3765fad`;
+  const [search, setSearch] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=a117932c0e837c0755546948f3765fad`;
 
   useEffect(() => {
     fetch(url)
     .then ((resp) => resp.json())
     .then ((data) => setCurrentWeather(data))
   }, [url]);
-
-  const inputHandler = (event) => {
-    setGetWeather(event.target.value);
-  };
   
   const submitHandler = () => {
-    setState(getWeather);
+    setSearch(getWeather);
   };
 
   return (
-    <div>
-      <div>
-        <label>Enter Location: </label>
+    <div className="weatherSection">
+      <div className="">
+        <label>City: </label>
         <input
           type="text"
-          onChange={inputHandler}
+          onChange={(event) => setGetWeather(event.target.value)}
           value={getWeather}
         />
         <button onClick={submitHandler}>Search</button>
       </div>
       <div className="weather">
         {currentWeather.main ? (
-          <div>
-            <p>{currentWeather.name}</p>
-            {/* <img alt='clouds'>{currentWeather.weather[0].icon}</img> */}
-            <p>{currentWeather.main.temp}&deg;F</p>
-            <p>{currentWeather.weather[0].main}- {currentWeather.weather[0].description}</p>
+          <div className="weatherResults">
+            <p id="city">{currentWeather.name}</p>
+            <p>{currentWeather.weather[0].main}</p>
+            <img
+              src={`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png`}
+              alt="weather icon"
+              className="weatherIcon"
+            />
+            <p id="temp">{currentWeather.main.temp}&deg;F</p>
+            <p>H: {currentWeather.main.temp_max}&deg;F L: {currentWeather.main.temp_min}&deg;F</p>
           </div>
         ) : (
-            <h2>Search a city's weather!</h2>
+            <h3>Search a city's weather!</h3>
         )}
       </div>
     </div>
